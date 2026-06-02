a5d6dc766609c24fcb8df8daa73a2a6b7d3f096e
Son-chang-ki <aerof22@gmail.com>
2026-06-02 18:20:06 +0900

    days

---
+++ track006_spring/spring002_db_mysql/src/main/java/com/the703/dao/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.the703.dao;
+
+import java.util.List;
+
+import com.the703.dto.UserInfoDto;
+
+//userinfo-mapper.xml
+//1. com.the703.dto  > lombok 이용해서 UserInfoDto 만들기
+@Mapper
+public interface UserInfoMapper {
+	public int insert(UserInfoDto dto);
+	public List <UserInfoDto>selectAll();
+	public UserInfoDto select(int no);
+	public int update(UserInfoDto dto);
+	public int delete(int no);
+
+}
